648d763b922cb5617fa9ca21792708ac7b7c649d
yarblade <yarblade@gmail.com>
2015-12-11 18:01:29 +0300

    First version of Topshelf.Quartz.Unity

---
+++ Topshelf.Quartz.Unity/Extensions/HostConfiguratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Unity;
+
+using Quartz.Unity.UnityExtensions;
+
+using Topshelf.HostConfigurators;
+using Topshelf.Quartz.Unity.WindowsServices;
+using Topshelf.Unity.Extensions;
+
+namespace Topshelf.Quartz.Unity.Extensions
+{
+    public static class HostConfiguratorExtensions
+    {
+        public static HostConfigurator RunQuartzService(this HostConfigurator configurator, IUnityContainer container)
+        {
+            container.AddNewExtension<QuartzUnityExtension>();
+
+            configurator.RunService<QuartzService>(container);
+
+            return configurator;
+        }
+    }
+}
---
+++ Topshelf.Quartz.Unity/WindowsServices/QuartzService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Common.Logging;
+
+using Quartz;
+
+using Topshelf.Unity.WindowsServices;
+
+namespace Topshelf.Quartz.Unity.WindowsServices
+{
+    internal class QuartzService : IWindowsService
+    {
+        private readonly ILog _log = LogManager.GetLogger<QuartzService>();
+        private readonly IScheduler _scheduler;
+
+        public QuartzService(IScheduler scheduler)
+        {
+            _scheduler = scheduler;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                _scheduler.Start();
+            }
+            catch (Exception ex)
+            {
+                _log.FatalFormat("Scheduler start failed: {0}.", ex, ex.Message);
+                throw;
+            }
+
+            _log.Info("Scheduler started successfully.");
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                _scheduler.Shutdown(true);
+            }
+            catch (Exception ex)
+            {
+                _log.ErrorFormat("Scheduler start failed: {0}.", ex, ex.Message);
+                throw;
+            }
+
+            _log.Info("Scheduler shutdown complete.");
+        }
+    }
+}
